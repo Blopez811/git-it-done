0a304e2e414422bdf519ce1946ac7811a1fe4932
Benicio Lopez <benicio.lopez@gmail.com>
2021-07-28 09:28:18 -0500

    fixed error need to add css classes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -33,6 +33,25 @@ var displayRepos = function(repos, searchTerm) {
     console.log(searchTerm);
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
+    //loop over repos
+    for(var i=0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        //create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item- flex-row justify-space-between align-center";
+
+        //create a span eement to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        //append to container
+        repoEl.appendChild(titleEl);
+
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
